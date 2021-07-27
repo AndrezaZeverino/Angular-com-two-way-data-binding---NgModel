24137040219790624890831954371a2d15736e73
Andreza Zeverino <82824036+AndrezaZeverino@users.noreply.github.com>
2021-07-27 16:21:24 -0300

    Add files via upload

---
+++ app/model/person.ts
@@ -0,0 +1,10 @@
+export class Person {
+    nome: string = "";
+    idade: number = 0;
+    email: string = "";
+    rua: string = "";
+    numero: number = 0;
+    bairro: string = "";
+    cidade: string = "";
+    estado: string = "";
+}
